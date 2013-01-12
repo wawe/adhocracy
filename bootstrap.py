6390e5976f67bfbd6a66580d483671accc458028
Joscha Krutzki <joka@jokasis.de>
2013-01-12 20:02:13 +0100

    updated bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -134,9 +134,7 @@ parser.add_option("-c", None, action="store", dest="config_file",
                    help=("Specify the path to the buildout configuration "
                          "file to be used."))
 
-options, orig_args = parser.parse_args()
-
-args = []
+options, args = parser.parse_args()
 
 # if -c was provided, we push it back into args for buildout's main function
 if options.config_file is not None:
@@ -155,6 +153,7 @@ if options.setup_source is None:
 
 if options.accept_buildout_test_releases:
     args.append('buildout:accept-buildout-test-releases=true')
+args.append('bootstrap')
 
 try:
     import pkg_resources
@@ -258,9 +257,6 @@ if exitcode != 0:
 ws.add_entry(eggs_dir)
 ws.require(requirement)
 import zc.buildout.buildout
-if orig_args:
-    # run buildout with commands passed to bootstrap.py, then actually bootstrap
-    zc.buildout.buildout.main(args + orig_args)
-zc.buildout.buildout.main(args + ['bootstrap'])
+zc.buildout.buildout.main(args)
 if not options.eggs:  # clean up temporary egg directory
     shutil.rmtree(eggs_dir)
