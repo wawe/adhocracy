65a0c33f8d6450d2d483d529bdb8dbbba62c7724
Joscha Krutzki <joka@jokasis.de>
2013-01-27 18:45:07 +0100

    hierachical badges helper

--- adhocracy/lib/helpers/badge_helper.py
+++ adhocracy/lib/helpers/badge_helper.py
@@ -10,17 +10,31 @@ def generate_thumbnail_tag(badge, height="48", width="48"):
     width = width
     #TODO resize and get mimetype from PIL, joka
     #TODO cache, joka
+    #TODO test, joka
     #TODO config option to set default width/height, joka
+    #TODO what todo if no colour and no thumbnail?, joka
     mimetype = "image/png"
     img_template = """<img src="data:%s;base64,%s" height="%s" width="%s" />"""
     data = badge.thumbnail
+    colour = badge.color or u"#ffffff"
     if not data:
         im = Image.new('RGB', (10, 10))
         draw = ImageDraw.Draw(im)
-        draw.rectangle((0,0,10,10), fill=badge.color, outline=badge.color)
+        draw.rectangle((0, 0, 10, 10), fill=colour, outline=colour)
         f = StringIO.StringIO()
         im.save(f, "PNG")
         data = f.getvalue()
         del draw, im, f
     data_enc = b64encode(data)
     return (img_template % (mimetype, data_enc, heigth, width))
+
+
+def get_parent_badges(badge):
+    """Returns a generator with all parent badges
+       in hierachical order (root last)
+    """
+    if hasattr(badge, "parent") and badge.parent:
+        parent = badge.parent
+        yield parent
+        for p in get_parent_badges(parent):
+            yield p
